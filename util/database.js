c6f72d85b5ba916edad5eed95f835e454de245f1
lvl mja <84472102+Eve-SatOrU@users.noreply.github.com>
2023-12-27 23:51:58 +0100

    done

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    dialect: process.env.DB_DIALECT,
+    dialect: 'mysql',
     host: process.env.DB_HOST,
 });
 
