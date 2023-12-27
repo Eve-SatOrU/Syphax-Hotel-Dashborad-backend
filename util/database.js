37450548b9aaced270a64c32d3a4bbf7e2e988be
lvl mja <84472102+Eve-SatOrU@users.noreply.github.com>
2023-12-27 22:56:26 +0100

    done

--- util/database.js
+++ util/database.js
@@ -1,8 +1,9 @@
-// this to connect our app in mySQL
+require('dotenv').config();
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('hotel', 'root', '',
-    {
-        dialect: 'mysql',
-        host: 'Localhost'
-    });
-module.exports = sequelize; 
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+});
+
+module.exports = sequelize;
