2805aa142506aa7eb13a4f32cbe27395dd5f4a37
lvl mja <84472102+Eve-SatOrU@users.noreply.github.com>
2023-12-28 13:14:02 +0100

    done

--- util/database.js
+++ util/database.js
@@ -4,6 +4,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: 'mysql',
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
 });
 
 module.exports = sequelize;
