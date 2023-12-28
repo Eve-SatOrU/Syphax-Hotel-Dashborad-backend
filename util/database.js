cd98c9352ccf0f25df4001e03ab69cb84e6c45c3
lvl mja <84472102+Eve-SatOrU@users.noreply.github.com>
2023-12-28 15:25:34 +0100

    done

--- util/database.js
+++ util/database.js
@@ -4,11 +4,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: 'mysql',
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    ssl: {
-        // Assuming you want to use SSL
-        rejectUnauthorized: true,
-      },    
+    port: process.env.DB_PORT, 
 });
 
 module.exports = sequelize;
